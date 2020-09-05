d94e4ed03632b15da80df7f6287d1e135a1a89f1
scalarion <hs@scalarion.com>
2020-09-05 19:38:17 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,30 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-USERNAME=`git config --list | awk -F= '$1=="user.name"{print $2}'`
-read -p "git user.name [${USERNAME}]: " input
-USERNAME=${input:-${USERNAME}}
-echo $USERNAME
+USERNAME=$(git config --list | awk -F= '$1=="user.name"{print $2}')
+read -e -p "git user.name [${USERNAME}]: " name && USERNAME=${name:-${USERNAME}}
+if [ -z ${USERNAME} ]; then echo "git user.name must not be empty" && exit 0; fi
 
-if [ -z ${USERNAME} ]; then echo "var is unset"; else echo "var is set to '$USERNAME'"; fi
+USEREMAIL=$(git config --list | awk -F= '$1=="user.email"{print $2}')
+read -e -p "git user.email [${USEREMAIL}]: " mail && USEREMAIL=${mail:-${USEREMAIL}}
+if [ -z ${USEREMAIL} ]; then echo "git user.email must not be empty" && exit 0; fi
 
+git config --global user.name $USERNAME
+git config --global user.email $USEREMAIL
 
-#sudo dpkg --add-architecture i386
+sudo dpkg --add-architecture i386
 
-#sudo apt-get update
+sudo apt-get update
 
-#sudo apt-get install reposudo apt-get install android-tools-adb android-tools-fastboot autoconf \
-#        automake bc bison build-essential ccache cscope curl device-tree-compiler \
-#        expect flex ftp-upload gdisk iasl libattr1-dev libc6:i386 libcap-dev \
-#        libfdt-dev libftdi-dev libglib2.0-dev libhidapi-dev libncurses5-dev \
-#        libpixman-1-dev libssl-dev libstdc++6:i386 libtool libz1:i386 make \
-#        mtools netcat python-crypto python3-crypto python-pyelftools \
-#        python3-pycryptodome python3-pyelftools python-serial python3-serial \
-#        rsync unzip uuid-dev xdg-utils xterm xz-utils zlib1g-dev make repo
+sudo apt-get install reposudo apt-get install android-tools-adb android-tools-fastboot autoconf \
+        automake bc bison build-essential ccache cscope curl device-tree-compiler \
+        expect flex ftp-upload gdisk iasl libattr1-dev libc6:i386 libcap-dev \
+        libfdt-dev libftdi-dev libglib2.0-dev libhidapi-dev libncurses5-dev \
+        libpixman-1-dev libssl-dev libstdc++6:i386 libtool libz1:i386 make \
+        mtools netcat python-crypto python3-crypto python-pyelftools \
+        python3-pycryptodome python3-pyelftools python-serial python3-serial \
+        rsync unzip uuid-dev xdg-utils xterm xz-utils zlib1g-dev make repo
 
-#mkdir -p ~/optee/qemu
-#cd ~/optee/qemu
-#repo init -u https://github.com/OP-TEE/manifest.git -b 3.10.0
-#repo sync
-#cd build
-#make toolchains -j2
-#make run
+mkdir -p ~/optee/qemu
+cd ~/optee/qemu
+repo init -u https://github.com/OP-TEE/manifest.git -b 3.10.0
+sudo cp ./.repo/repo/repo /usr/bin/repo
+repo sync
+cd build
+make toolchains -j2
+make run
