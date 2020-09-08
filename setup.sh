0081054f8d821993c0be5782664632fcb5ced067
scalarion <hs@scalarion.com>
2020-09-08 19:46:17 +0200

    update

--- setup.sh
+++ setup.sh
@@ -4,15 +4,16 @@
 if [ -z "$1" ] && [ -z "$2" ]; then
     USERNAME=$(git config --list | awk -F= '$1=="user.name"{print $2}')
     read -e -p "git user.name [${USERNAME}]: " name && USERNAME=${name:-${USERNAME}}
-    if [ -z ${USERNAME} ]; then echo "git user.name must not be empty" && exit 1; fi
 
     USEREMAIL=$(git config --list | awk -F= '$1=="user.email"{print $2}')
     read -e -p "git user.email [${USEREMAIL}]: " mail && USEREMAIL=${mail:-${USEREMAIL}}
-    if [ -z ${USEREMAIL} ]; then echo "git user.email must not be empty" && exit 1; fi
 else
     USERNAME = $1
     USERNAME = $2
 fi
+
+if [ -z ${USERNAME} ]; then echo "git user.name must not be empty" && exit 1; fi
+if [ -z ${USEREMAIL} ]; then echo "git user.email must not be empty" && exit 1; fi
 
 echo "setting up git ... " && \
 git config --global user.name $USERNAME && \
